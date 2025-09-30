cbfdfa10be932de86906c325139424a3bc90d693
Lilit <baregamyanlilit36@gmail.com>
2025-10-01 00:36:51 +0400

    first commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import JobList from "./JobList";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import PaginationControls from "./PaginationControls";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <ResultsCount/>
+        <SortingControls/>
+      </div>
+      <JobList/>
+      <PaginationControls/>
+    </div>
+  );
+}
