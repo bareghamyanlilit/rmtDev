fac61e508bb1def49647930f8290b46027eed6d1
Lilit <baregamyanlilit36@gmail.com>
2025-10-10 23:34:13 +0400

    pagination 159]

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,67 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type PaginbationControlsProps = {
+  onClick: (direction: "next" | "previous") => void;
+  totalNumberOfPages: number;
+  currentPage: number;
+};
+
+export default function PaginationControls({
+  onClick,
+  totalNumberOfPages,
+  currentPage,
+}: PaginbationControlsProps) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <PaginqationButton
+          direction="previous"
+          currentPage={currentPage}
+          onClick={() => onClick("previous")}
+        />
+      )}
+      {currentPage < totalNumberOfPages && (
+        <PaginqationButton
+          direction="next"
+          currentPage={currentPage}
+          onClick={() => onClick("next")}
+        />
+      )}
+    </section>
+  );
+}
+
+type PaginationButtonProps = {
+  direction: "next" | "previous";
+  currentPage: number;
+  onClick: () => void;
+};
+
+function PaginqationButton({
+  direction,
+  currentPage,
+  onClick,
+}: PaginationButtonProps) {
+  return (
+    <button
+      onClick={(e) => {
+        onClick();
+        e.currentTarget.blur();
+      }}
+      className={`pagination__button pagination_button--${direction}`}
+    >
+      {direction === "previous" && (
+        <>
+          Page {currentPage - 1}
+          <ArrowLeftIcon />
+        </>
+      )}
+      {direction === "next" && (
+        <>
+          Page {currentPage + 1}
+          <ArrowRightIcon />
+        </>
+      )}
+    </button>
+  );
 }
